8e8d6058a912082ecb8fb38243252e035996f15d
jbiblio <michael@jbiblio.de>
2021-08-03 11:57:08 +0200

    Migrate to JUnit5 and MockMvc

--- src/test/java/io/zeebe/monitor/rest/ViewControllerTest.java
+++ src/test/java/io/zeebe/monitor/rest/ViewControllerTest.java
@@ -1,47 +1,49 @@
 package io.zeebe.monitor.rest;
 
-import io.zeebe.monitor.ZeebeSimpleMonitorApp;
-import io.zeebe.monitor.repository.*;
+import io.zeebe.monitor.repository.ElementInstanceRepository;
+import io.zeebe.monitor.repository.ErrorRepository;
+import io.zeebe.monitor.repository.HazelcastConfigRepository;
+import io.zeebe.monitor.repository.IncidentRepository;
+import io.zeebe.monitor.repository.JobRepository;
+import io.zeebe.monitor.repository.MessageRepository;
+import io.zeebe.monitor.repository.MessageSubscriptionRepository;
+import io.zeebe.monitor.repository.ProcessInstanceRepository;
+import io.zeebe.monitor.repository.ProcessRepository;
+import io.zeebe.monitor.repository.TimerRepository;
+import io.zeebe.monitor.repository.VariableRepository;
 import io.zeebe.monitor.zeebe.ZeebeHazelcastService;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.CoreMatchers.containsString;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        properties = {
+                "white-label.logo.path: img/test-logo.png",
+                "white-label.custom.title: Test Zeebe Simple Monitor",
+                "white-label.custom.css.path: css/test-custom.css",
+                "white-label.custom.js.path: js/test-custom.js",
+        })
 @AutoConfigureMockMvc
-@SpringBootTest(classes = ZeebeSimpleMonitorApp.class,
-    webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
-    properties = {
-        "white-label.logo.path: img/test-logo.png",
-        "white-label.custom.title: Test Zeebe Simple Monitor",
-        "white-label.custom.css.path: css/test-custom.css",
-        "white-label.custom.js.path: js/test-custom.js",
-    })
 public class ViewControllerTest {
-
-  @LocalServerPort
-  private int port;
 
   @Autowired
   private ViewController controller;
 
   @Autowired
-  private TestRestTemplate restTemplate;
+  private MockMvc mockMvc;
 
   @MockBean
   private HazelcastConfigRepository hazelcastConfigRepository;
@@ -68,44 +70,39 @@ public class ViewControllerTest {
   @MockBean
   private ErrorRepository errorRepository;
 
-  @Before
+  @BeforeEach
   public void setUp() throws Exception {
     when(processRepository.findAll(any(Pageable.class))).thenReturn(Page.empty());
   }
 
   @Test
-  public void index_page_successfully_responded() {
-    ResponseEntity<String> entity = restTemplate.getForEntity("/", String.class);
-
-    assertThat(entity.getStatusCode()).isEqualTo(HttpStatus.OK);
+  public void index_page_successfully_responded() throws Exception {
+    mockMvc.perform(get("/"))
+            .andExpect(status().isOk());
   }
 
   @Test
-  public void index_page_contains_whitelabeling_title() {
-    ResponseEntity<String> entity = restTemplate.getForEntity("/", String.class);
-
-    assertThat(entity.getBody()).contains("Test Zeebe Simple Monitor");
+  public void index_page_contains_whitelabeling_title() throws Exception {
+    mockMvc.perform(get("/"))
+            .andExpect(content().string(containsString("Test Zeebe Simple Monitor")));
   }
 
   @Test
-  public void index_page_contains_whitelabeling_logo() {
-    ResponseEntity<String> entity = restTemplate.getForEntity("/", String.class);
-
-    assertThat(entity.getBody()).contains("<img src=\"/img/test-logo.png\"");
+  public void index_page_contains_whitelabeling_logo() throws Exception {
+    mockMvc.perform(get("/"))
+            .andExpect(content().string(containsString("<img src=\"/img/test-logo.png\"")));
   }
 
   @Test
-  public void index_page_contains_whitelabeling_js() {
-    ResponseEntity<String> entity = restTemplate.getForEntity("/", String.class);
+  public void index_page_contains_whitelabeling_js() throws Exception {
+    mockMvc.perform(get("/"))
+            .andExpect(content().string(containsString("<script src=\"/js/test-custom.js\"")));
 
-    assertThat(entity.getBody()).contains("<script src=\"/js/test-custom.js\"></script>");
   }
 
   @Test
-  public void index_page_contains_whitelabeling_css() {
-    ResponseEntity<String> entity = restTemplate.getForEntity("/", String.class);
-
-    assertThat(entity.getBody()).contains("<link rel=\"stylesheet\" type=\"text/css\" href=\"/css/test-custom.css\"/>");
+  public void index_page_contains_whitelabeling_css() throws Exception {
+    mockMvc.perform(get("/"))
+            .andExpect(content().string(containsString("<link rel=\"stylesheet\" type=\"text/css\" href=\"/css/test-custom.css\"/>")));
   }
-
 }
