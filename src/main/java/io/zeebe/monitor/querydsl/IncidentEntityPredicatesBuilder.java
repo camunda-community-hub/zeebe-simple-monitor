58a6aafcc4e964fab93c2519ba20300bdbd07ecd
nitram509 <maki@bitkings.de>
2022-10-05 09:32:21 +0200

    feat(ui) add incident search filter for created before and after #438

--- src/main/java/io/zeebe/monitor/querydsl/IncidentEntityPredicatesBuilder.java
+++ src/main/java/io/zeebe/monitor/querydsl/IncidentEntityPredicatesBuilder.java
@@ -6,14 +6,19 @@ import com.querydsl.core.types.dsl.Expressions;
 import com.querydsl.core.types.dsl.PathBuilder;
 import io.zeebe.monitor.entity.QIncidentEntity;
 
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeParseException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 import static org.apache.commons.lang3.StringUtils.isEmpty;
 
 public class IncidentEntityPredicatesBuilder {
   final PathBuilder<QIncidentEntity> pathBuilder = new PathBuilder<>(QIncidentEntity.class, QIncidentEntity.incidentEntity.getMetadata());
-  private List<Predicate> predicates = new ArrayList<>();
+  private final List<Predicate> predicates = new ArrayList<>();
 
   public IncidentEntityPredicatesBuilder onlyUnresolved() {
     predicates.add(pathBuilder.getNumber("resolved", Long.class).isNull());
@@ -34,11 +39,38 @@ public class IncidentEntityPredicatesBuilder {
     return this;
   }
 
-  public BooleanExpression build() {
+  public IncidentEntityPredicatesBuilder createdAfter(String timestamp) {
+    if (!isEmpty(timestamp)) {
+      final Optional<Long> created = parseIsoToUtcMillis(timestamp);
+      created.ifPresent(utcMillis -> predicates.add(pathBuilder.getNumber("created", Long.class).goe(utcMillis)));
+    }
+    return this;
+  }
+
+  public IncidentEntityPredicatesBuilder createdBefore(String timestamp) {
+    if (!isEmpty(timestamp)) {
+      final Optional<Long> created = parseIsoToUtcMillis(timestamp);
+      created.ifPresent(utcMillis -> predicates.add(pathBuilder.getNumber("created", Long.class).loe(utcMillis)));
+    }
+    return this;
+  }
+
+  public Predicate build() {
     BooleanExpression result = Expressions.asBoolean(true).isTrue();
     for (Predicate predicate : predicates) {
       result = result.and(predicate);
     }
     return result;
+  }
+
+  private Optional<Long> parseIsoToUtcMillis(String timestamp) {
+    try {
+      final ZonedDateTime zonedDateTime = ZonedDateTime.from(DateTimeFormatter.ISO_DATE_TIME.parse(timestamp));
+      final long utcMillis = zonedDateTime.withZoneSameInstant(ZoneId.of("UTC")).toInstant().toEpochMilli();
+      return Optional.of(utcMillis);
+    } catch (DateTimeParseException ignore) {
+      // ignore
+    }
+    return Optional.empty();
   }
 }
--- src/main/java/io/zeebe/monitor/rest/IncidentsViewController.java
+++ src/main/java/io/zeebe/monitor/rest/IncidentsViewController.java
@@ -1,6 +1,6 @@
 package io.zeebe.monitor.rest;
 
-import com.querydsl.core.types.dsl.BooleanExpression;
+import com.querydsl.core.types.Predicate;
 import io.zeebe.monitor.entity.IncidentEntity;
 import io.zeebe.monitor.querydsl.IncidentEntityPredicatesBuilder;
 import io.zeebe.monitor.repository.IncidentRepository;
@@ -28,15 +28,19 @@ public class IncidentsViewController extends AbstractViewController {
   public String incidentList(final Map<String, Object> model,
                              final Pageable pageable,
                              @RequestParam(required = false) String bpmnProcessId,
-                             @RequestParam(required = false) String errorType) {
+                             @RequestParam(required = false) String errorType,
+                             @RequestParam(required = false) String createdAfter,
+                             @RequestParam(required = false) String createdBefore) {
 
-    final IncidentEntityPredicatesBuilder predicatesBuilder = new IncidentEntityPredicatesBuilder();
-    predicatesBuilder.onlyUnresolved();
-    predicatesBuilder.withProcessId(bpmnProcessId);
-    predicatesBuilder.withErrorType(errorType);
-    final BooleanExpression predicates = predicatesBuilder.build();
+    final Predicate predicate = new IncidentEntityPredicatesBuilder()
+        .onlyUnresolved()
+        .withProcessId(bpmnProcessId)
+        .withErrorType(errorType)
+        .createdAfter(createdAfter)
+        .createdBefore(createdBefore)
+        .build();
 
-    final Page<IncidentEntity> dtos = incidentRepository.findAll(predicates, pageable);
+    final Page<IncidentEntity> dtos = incidentRepository.findAll(predicate, pageable);
     final List<IncidentListDto> incidents = new ArrayList<>();
     for (final IncidentEntity incidentEntity : dtos) {
       final IncidentListDto dto = toDto(incidentEntity);
