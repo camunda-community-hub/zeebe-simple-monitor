6da0dcad0fe50f616228019f22542101f31e6b47
Yannick Schmetz <yannick.schmetz@inform-software.com>
2022-10-28 15:59:06 +0200

    Adds cors support

--- src/main/java/io/zeebe/monitor/WebSocketConfig.java
+++ src/main/java/io/zeebe/monitor/WebSocketConfig.java
@@ -1,14 +1,19 @@
 package io.zeebe.monitor;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import java.util.ArrayList;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+  @Value("${spring.data.web.allowedOriginsUrls}")
+  private String allowedOriginsUrls;
+
   @Override
   public void configureMessageBroker(MessageBrokerRegistry config) {
     config.enableSimpleBroker("/");
@@ -17,6 +22,11 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
   @Override
   public void registerStompEndpoints(StompEndpointRegistry registry) {
-    registry.addEndpoint("/notifications").withSockJS();
+    String[] allowedOriginsUrlArr = this.allowedOriginsUrls.split(";");
+    if(allowedOriginsUrlArr.length>0){
+        registry.addEndpoint("/notifications").setAllowedOrigins(allowedOriginsUrlArr).withSockJS();
+    }else{
+        registry.addEndpoint("/notifications").withSockJS();
+    }
   }
 }
