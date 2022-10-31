163f5ef46c9cdc96230f159bf6682a92cd18490c
Yannick Schmetz <yannick.schmetz@inform-software.com>
2022-10-31 08:39:32 +0100

    Move allowedOriginsUrls from spring to server

--- src/main/java/io/zeebe/monitor/WebSocketConfig.java
+++ src/main/java/io/zeebe/monitor/WebSocketConfig.java
@@ -11,7 +11,7 @@ import java.util.ArrayList;
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
-  @Value("${spring.data.web.allowedOriginsUrls}")
+  @Value("${spring.web.data.allowedOriginsUrls}")
   private String allowedOriginsUrls;
 
   @Override
