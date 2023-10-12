09254e69d992bcae894e8bf27029f1c6b53be02b
Lachlan Birdsey <lachlan.birdsey@elisa.fi>
2023-10-12 11:48:45 +0300

    fix: broken func

--- src/main/java/io/zeebe/monitor/rest/InstancesAuditLogViewController.java
+++ src/main/java/io/zeebe/monitor/rest/InstancesAuditLogViewController.java
@@ -76,7 +76,7 @@ public class InstancesAuditLogViewController extends AbstractInstanceViewControl
                   entry.setElementId(e.getElementId());
                   entry.setElementName(flowElements.getOrDefault(e.getElementId(), ""));
                   entry.setBpmnElementType(e.getBpmnElementType().name());
-                  entry.setState(e.getIntent());
+                  entry.setState(e.getIntent().name());
                   entry.setTimestamp(Instant.ofEpochMilli(e.getTimestamp()).toString());
 
                   return entry;
