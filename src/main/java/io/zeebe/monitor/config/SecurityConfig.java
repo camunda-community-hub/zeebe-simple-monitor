aa7a6e1600d5f20d84f09e3666f3faa1b036eb85
hamoud <hkhamis@oceaneering.com>
2024-01-08 17:42:04 +0100

    feat: add authentication with Keycloak

---
+++ src/main/java/io/zeebe/monitor/config/SecurityConfig.java
@@ -0,0 +1,58 @@
+package io.zeebe.monitor.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
+import org.springframework.security.core.session.SessionRegistryImpl;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
+import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig {
+  private final KeycloakLogoutHandler keycloakLogoutHandler;
+  SecurityConfig(KeycloakLogoutHandler keycloakLogoutHandler) {
+    this.keycloakLogoutHandler = keycloakLogoutHandler;
+  }
+  @Bean
+  protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
+    return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
+  }
+  @Order(1)
+  @Bean
+  public SecurityFilterChain clientFilterChain(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+    .anyRequest()
+    .authenticated();
+    http.oauth2Login()
+    .and()
+    .logout()
+    .addLogoutHandler(keycloakLogoutHandler)
+    .logoutSuccessUrl("/");
+    return http.build();
+  }
+
+  @Order(2)
+  @Bean
+  public SecurityFilterChain resourceServerFilterChain(HttpSecurity http) throws Exception {
+    http.authorizeRequests()
+    .requestMatchers(new AntPathRequestMatcher("/customers*"))
+    .hasRole("USER")
+    .anyRequest()
+    .authenticated();
+    http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
+    return http.build();
+  }
+  @Bean
+  public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
+    return http.getSharedObject(AuthenticationManagerBuilder.class)
+    .build();
+  }
+}
