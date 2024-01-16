14959d879a6bec1e46a9a8263c825418fc6f35f4
hamoud <hkhamis@oceaneering.com>
2024-01-16 15:06:12 +0100

    chore: update SecurityConfig and conditionally enable spring security

--- src/main/java/io/zeebe/monitor/config/KeycloakLogoutHandler.java
+++ src/main/java/io/zeebe/monitor/config/KeycloakLogoutHandler.java
@@ -1,19 +1,16 @@
 package io.zeebe.monitor.config;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.oauth2.core.oidc.user.OidcUser;
 import org.springframework.security.web.authentication.logout.LogoutHandler;
-import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-
-@Component
 public class KeycloakLogoutHandler implements LogoutHandler {
 
   private static final Logger logger = LoggerFactory.getLogger(KeycloakLogoutHandler.class);
@@ -43,5 +40,4 @@ public class KeycloakLogoutHandler implements LogoutHandler {
       logger.error("Could not propagate logout to Keycloak");
     }
   }
-
 }
--- src/main/java/io/zeebe/monitor/config/SecurityConfig.java
+++ src/main/java/io/zeebe/monitor/config/SecurityConfig.java
@@ -1,58 +1,41 @@
 package io.zeebe.monitor.config;
 
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.annotation.Order;
-import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.oauth2.server.resource.OAuth2ResourceServerConfigurer;
-import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
-import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+import org.springframework.security.web.authentication.logout.LogoutHandler;
+import org.springframework.web.client.RestTemplate;
 
 @Configuration
+@ConditionalOnProperty(
+prefix = "spring",
+name = "security.enabled",
+havingValue = "true")
 @EnableWebSecurity
-class SecurityConfig {
-  private final KeycloakLogoutHandler keycloakLogoutHandler;
-  SecurityConfig(KeycloakLogoutHandler keycloakLogoutHandler) {
-    this.keycloakLogoutHandler = keycloakLogoutHandler;
-  }
+public class SecurityConfig {
   @Bean
-  protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
-    return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
-  }
-  @Order(1)
-  @Bean
-  public SecurityFilterChain clientFilterChain(HttpSecurity http) throws Exception {
-    http.authorizeRequests()
-    .anyRequest()
-    .authenticated();
-    http.oauth2Login()
-    .and()
-    .logout()
-    .addLogoutHandler(keycloakLogoutHandler)
-    .logoutSuccessUrl("/");
-    return http.build();
+  RestTemplate restTemplate() {
+    return new RestTemplate();
   }
 
-  @Order(2)
   @Bean
-  public SecurityFilterChain resourceServerFilterChain(HttpSecurity http) throws Exception {
-    http.authorizeRequests()
-    .requestMatchers(new AntPathRequestMatcher("/customers*"))
-    .hasRole("USER")
-    .anyRequest()
-    .authenticated();
-    http.oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt);
-    return http.build();
+  LogoutHandler logoutHandler(final RestTemplate restTemplate) {
+    return new KeycloakLogoutHandler(restTemplate);
   }
+
   @Bean
-  public AuthenticationManager authenticationManager(HttpSecurity http) throws Exception {
-    return http.getSharedObject(AuthenticationManagerBuilder.class)
-    .build();
+  public SecurityFilterChain securityFilterChain(HttpSecurity http, LogoutHandler logoutHandler) throws Exception {
+    http
+    .authorizeHttpRequests(authorize -> authorize
+    .requestMatchers("/actuator/health/**").permitAll()
+    .anyRequest().authenticated()
+    )
+    .oauth2Login(Customizer.withDefaults())
+    .logout(oauth2 -> oauth2.addLogoutHandler(logoutHandler).logoutSuccessUrl("/"));
+    return http.build();
   }
 }
