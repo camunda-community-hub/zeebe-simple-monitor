a147f9a7cc95e402547179e6dbbe37d33147f838
Lachlan Birdsey <lachlan.birdsey@elisa.fi>
2024-04-23 08:38:27 +0300

    fix: prevent duplicate writes for message subscriptions

--- src/main/java/io/zeebe/monitor/zeebe/ZeebeImportService.java
+++ src/main/java/io/zeebe/monitor/zeebe/ZeebeImportService.java
@@ -19,76 +19,83 @@ import static io.zeebe.monitor.zeebe.util.ImportUtil.ifEvent;
 @Component
 public class ZeebeImportService {
 
-  @Autowired private ProcessAndElementImporter processAndElementImporter;
-  @Autowired private VariableImporter variableImporter;
-  @Autowired private JobImporter jobImporter;
-  @Autowired private IncidentImporter incidentImporter;
-  @Autowired private MessageImporter messageImporter;
-  @Autowired private MessageSubscriptionImporter messageSubscriptionImporter;
-  @Autowired private TimerImporter timerImporter;
-  @Autowired private ErrorImporter errorImporter;
+    @Autowired
+    private ProcessAndElementImporter processAndElementImporter;
+    @Autowired
+    private VariableImporter variableImporter;
+    @Autowired
+    private JobImporter jobImporter;
+    @Autowired
+    private IncidentImporter incidentImporter;
+    @Autowired
+    private MessageImporter messageImporter;
+    @Autowired
+    private MessageSubscriptionImporter messageSubscriptionImporter;
+    @Autowired
+    private TimerImporter timerImporter;
+    @Autowired
+    private ErrorImporter errorImporter;
 
-  @Autowired private HazelcastConfigRepository hazelcastConfigRepository;
+    @Autowired
+    private HazelcastConfigRepository hazelcastConfigRepository;
 
-  public ZeebeHazelcast importFrom(final HazelcastInstance hazelcast) {
+    public ZeebeHazelcast importFrom(final HazelcastInstance hazelcast) {
 
-    final var hazelcastConfig =
-        hazelcastConfigRepository
-            .findById("cfg")
-            .orElseGet(
-                () -> {
-                  final var config = new HazelcastConfig();
-                  config.setId("cfg");
-                  config.setSequence(-1);
-                  return config;
-                });
+        final var hazelcastConfig =
+                hazelcastConfigRepository
+                        .findById("cfg")
+                        .orElseGet(
+                                () -> {
+                                    final var config = new HazelcastConfig();
+                                    config.setId("cfg");
+                                    config.setSequence(-1);
+                                    return config;
+                                });
 
-    final var builder =
-        ZeebeHazelcast.newBuilder(hazelcast)
-            .addProcessListener(
-                record -> ifEvent(record, Schema.ProcessRecord::getMetadata, processAndElementImporter::importProcess))
-            .addProcessInstanceListener(
-                record ->
-                    ifEvent(
-                        record,
-                        Schema.ProcessInstanceRecord::getMetadata,
-                        processAndElementImporter::importProcessInstance))
-            .addIncidentListener(
-                record -> ifEvent(record, Schema.IncidentRecord::getMetadata, incidentImporter::importIncident))
-            .addJobListener(
-                record -> ifEvent(record, Schema.JobRecord::getMetadata, jobImporter::importJob))
-            .addVariableListener(
-                record -> ifEvent(record, Schema.VariableRecord::getMetadata, variableImporter::importVariable))
-            .addTimerListener(
-                record -> ifEvent(record, Schema.TimerRecord::getMetadata, timerImporter::importTimer))
-            .addMessageListener(
-                record -> ifEvent(record, Schema.MessageRecord::getMetadata, messageImporter::importMessage))
-            .addMessageSubscriptionListener(
-                record ->
-                    ifEvent(
-                        record,
-                        Schema.MessageSubscriptionRecord::getMetadata,
-                            messageSubscriptionImporter::importMessageSubscription))
-            .addMessageStartEventSubscriptionListener(
-                record ->
-                    ifEvent(
-                        record,
-                        Schema.MessageStartEventSubscriptionRecord::getMetadata,
-                            messageSubscriptionImporter::importMessageStartEventSubscription))
-            .addErrorListener(errorImporter::importError)
-            .postProcessListener(
-                sequence -> {
-                  hazelcastConfig.setSequence(sequence);
-                  hazelcastConfigRepository.save(hazelcastConfig);
-                });
+        final var builder =
+                ZeebeHazelcast.newBuilder(hazelcast)
+                        .addProcessListener(
+                                record -> ifEvent(record, Schema.ProcessRecord::getMetadata, processAndElementImporter::importProcess))
+                        .addProcessInstanceListener(
+                                record ->
+                                        ifEvent(
+                                                record,
+                                                Schema.ProcessInstanceRecord::getMetadata,
+                                                processAndElementImporter::importProcessInstance))
+                        .addIncidentListener(
+                                record -> ifEvent(record, Schema.IncidentRecord::getMetadata, incidentImporter::importIncident))
+                        .addJobListener(
+                                record -> ifEvent(record, Schema.JobRecord::getMetadata, jobImporter::importJob))
+                        .addVariableListener(
+                                record -> ifEvent(record, Schema.VariableRecord::getMetadata, variableImporter::importVariable))
+                        .addTimerListener(
+                                record -> ifEvent(record, Schema.TimerRecord::getMetadata, timerImporter::importTimer))
+                        .addMessageListener(
+                                record -> ifEvent(record, Schema.MessageRecord::getMetadata, messageImporter::importMessage))
+                        .addMessageSubscriptionListener(
+                                record ->
+                                        ifEvent(
+                                                record,
+                                                Schema.MessageSubscriptionRecord::getMetadata, messageSubscriptionImporter::importMessageSubscription))
+                        .addMessageStartEventSubscriptionListener(
+                                record ->
+                                        ifEvent(
+                                                record,
+                                                Schema.MessageStartEventSubscriptionRecord::getMetadata, messageSubscriptionImporter::importMessageStartEventSubscription))
+                        .addErrorListener(errorImporter::importError)
+                        .postProcessListener(
+                                sequence -> {
+                                    hazelcastConfig.setSequence(sequence);
+                                    hazelcastConfigRepository.save(hazelcastConfig);
+                                });
 
-    if (hazelcastConfig.getSequence() >= 0) {
-      builder.readFrom(hazelcastConfig.getSequence());
-    } else {
-      builder.readFromHead();
+        if (hazelcastConfig.getSequence() >= 0) {
+            builder.readFrom(hazelcastConfig.getSequence());
+        } else {
+            builder.readFromHead();
+        }
+
+        return builder.build();
     }
-
-    return builder.build();
-  }
 
 }
